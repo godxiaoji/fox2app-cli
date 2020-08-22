3ebba293cf174671797edbcc3b5fccf73ef79c23
DESKTOP-4SPP55M\Travis <godxiaoji@vip.qq.com>
2020-08-22 23:41:17 +0800

    A cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,72 @@
 #!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const program = require('commander')
+// const exec = require('child_process').exec
+const { version } = require('../package.json')
+const download = require('download-git-repo')
+const shell = require("shelljs")
+
+function echoError(message) {
+  console.log(chalk.red('Error: ' + message))
+}
+
+function echoSuccess(message) {
+  console.log(chalk.green(message))
+}
+
+program.version(version)
+
+program
+  .command('init <name>')
+  .description('Init new project.')
+  .parse(process.argv)
+  .action(name => {
+    if (fs.existsSync(name)) {
+      const stat = fs.lstatSync(name)
+
+      if (stat.isDirectory()) {
+        echoError(`Folder "${path.resolve(name)}" already exists.`)
+        shell.exit(1)
+      }
+    }
+
+    if (!shell.which('git')) {
+      //在控制台输出内容
+      echoError('Sorry, this script requires git')
+      // 以退出码为code退出当前进程
+      shell.exit(1)
+    }
+
+    shell.echo('Pull template from git.')
+    download(
+      'https://github.com:godxiaoji/fox2app-template#master',
+      name,
+      err => {
+
+        try {
+          if (err) throw err
+
+          echoSuccess('Pull template success.')
+
+          shell.cd(name)
+          shell.echo('Install dependencies.')
+          shell.exec("npm install --registry=https://registry.npm.taobao.org")
+
+          echoSuccess('Install dependencies success.')
+
+          shell.echo(chalk.blue(`\nYou should: cd ${name} & npm run serve`))
+
+          shell.exit(0)
+
+        } catch (err) {
+          echoError(err.message)
+          shell.exit(1)
+        }
+
+      }
+    )
+  })
+
+program.parse(process.argv)
--- bin/service.js
+++ bin/service.js
@@ -1,60 +1,7 @@
 #!/usr/bin/env node
-const fs = require('fs')
-const path = require('path')
 const program = require('commander')
-const exec = require('child_process').exec
 const { version } = require('../package.json')
 
-const download = require('download-git-repo')
-
 program.version(version)
-
-program
-  .command('lint')
-  .description('eslint . --fix')
-  .action(() => {
-    exec('eslint . --fix', err => {
-      if (err) throw err
-      console.log('eslint fix!!')
-    })
-  })
-
-program
-  .command('init <name>')
-  .description('创建项目')
-  .parse(process.argv)
-  .action(name => {
-    const projectPath = path.resolve(name)
-    console.log(projectPath)
-
-    if (fs.existsSync(projectPath)) {
-      console.log(11111)
-      const stat = fs.lstatSync()
-
-      if (stat.isDirectory()) {
-        throw new Error(`${name} Folder exist.`)
-      }
-    }
-
-    // download(
-    //   'https://github.com:godxiaoji/fox2app-template#master',
-    //   'testtttt',
-    //   err => {
-    //     if (err) throw err
-
-    //   }
-    // )
-  })
-
-// program
-// .command('lint')
-// .description('eslint . --fix')
-// .action(argv => {
-//   // console.log(argv._name + ' start')
-//   exec('eslint . --fix', (err, stdout, stderr) => {
-//     if (err) throw err
-//     console.log('eslint fix!!')
-//   })
-// })
 
 program.parse(process.argv)
